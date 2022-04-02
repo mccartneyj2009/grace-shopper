c248fc0c6c45b8e0ddb71195f7fc35f2088e9c7f
Maxwell Dubin <maxdhs@gmail.com>
2022-04-02 09:50:42 -0400

    ssl

--- db/index.js
+++ db/index.js
@@ -1,7 +1,8 @@
 const { Client } = require("pg");
 
 const client = new Client(
-  process.env.DATABASE_URL || {
+  process.env.DATABASE_URL,
+  { ssl: true } || {
     password: process.env.DB_PASSWORD,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
