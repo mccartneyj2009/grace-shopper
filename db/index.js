5fcc853ae88539c3fea4b801f89b579f9acb2a15
Maxwell Dubin <maxdhs@gmail.com>
2022-04-02 09:45:05 -0400

    database-url

--- db/index.js
+++ db/index.js
@@ -1,10 +1,12 @@
 const { Client } = require("pg");
 
-const client = new Client({
-  password: process.env.DB_PASSWORD,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-});
+const client = new Client(
+  process.env.DATABASE_URL || {
+    password: process.env.DB_PASSWORD,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+  }
+);
 
 client.connect();
 
