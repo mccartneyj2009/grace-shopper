1f1cefb3b05df460f66aa61e86e93e0fa1126281
Anthony <antromano21@gmail.com>
2022-04-07 18:56:57 -0400

    client connection

--- db/seed.js
+++ db/seed.js
@@ -1,16 +1,6 @@
 require("dotenv").config();
 const client = require("./");
 
-const seedDB = async () => {
-  await client.query(`
-    DROP TABLE IF EXISTS products;
-
-    CREATE TABLE products (id SERIAL PRIMARY KEY, title VARCHAR(255) UNIQUE);
-
-    INSERT INTO products (title) VALUES ('Acer Nitro 5 Laptop');
-    `);
-
-  console.log("DB SEEDED.");
-};
+const seedDB = async () => {};
 
 seedDB();
