cbdebe02d1306eafe6fd960795664f75567ffe6d
James McCartney <mccartneyj2009@gmail.com>
2022-04-23 09:19:47 -0400

    merge

--- src/components/meat.js
+++ src/components/meat.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from "react";
 const Meat = ({ meats }) => {
     const [species, setSpecies] = useState({});
     const [selected, setSelected] = useState([]);
-
     const fetchMeatBySingleSpecies = async (species) => {
         const resp = await fetch(
             `http://localhost:3001/api/meats/species/${species}`
@@ -13,6 +12,7 @@ const Meat = ({ meats }) => {
 
         setSpecies(info);
     };
+
     useEffect(() => {
         let selectedMeats = meats.filter((meat) => meat.species === species);
         if (!selectedMeats.length) {
@@ -24,36 +24,13 @@ const Meat = ({ meats }) => {
     if (!meats.length) {
         return <></>;
     }
-    // return (
-    //   <>
-    //     <h1>We have the meats</h1>
-    //     <select
-    //       name="meatlist"
-    //       id="meatlist"
-    //       onChange={(e) => setSpecies(e.target.value)}
-    //     >
-    //       <option value="">Please Select Meat</option>
-    //       {meats.map((meat) => {
-    //         return (
-    //           <option key={meat.id} value={meat.species}>
-    //             {meat.species}{" "}
-    //           </option>
-    //         );
-    //       })}
-    //     </select></>
-    //      <div>
-    //       {selected.map((meat) => {
-    //         return (
-    //           <div key={meat.id}>
-    //             <h2>{meat.species}</h2>
-
     return (
         <>
             <h1>We have the meats</h1>
             <select
                 name="meatlist"
                 id="meatlist"
-                onChange={(e) => fetchMeatBySingleSpecies(e.target.value)}
+                onChange={(e) => setSpecies(e.target.value)}
             >
                 <option value="">Please Select Meat</option>
                 {meats.map((meat) => {
