b856df3c9c311e3f3f9a52022c0c1f811cf0b058
Anthony <antromano21@gmail.com>
2022-04-25 19:39:36 -0400

    merge

--- src/App.js
+++ src/App.js
@@ -4,76 +4,79 @@ import { Route, Routes } from "react-router-dom";
 import { Meat, Home, Navbar, Login, Register, Info } from "./components";
 
 const App = () => {
-    const [meats, setMeat] = useState([]);
-    const [user, setUser] = useState({});
-    const [token, setToken] = useState("");
+  const [meats, setMeat] = useState([]);
+  const [user, setUser] = useState({});
+  const [token, setToken] = useState("");
 
-    const fetchUser = async () => {
-        try {
-            const lstoken = localStorage.getItem("token");
-            if (lstoken) {
-                setToken(lstoken);
-            }
-            if (lstoken) {
-                const resp = await fetch(`http://localhost:3001/api/users/me`, {
-                    headers: {
-                        "Content-Type": "application/json",
-                        Authorization: `Bearer ${lstoken}`,
-                    },
-                });
-
-                const info = await resp.json();
-
-                if (info) {
-                    setUser(info.user);
-                }
-                console.log(info);
-                return info;
-            }
-        } catch (error) {
-            throw error;
-        }
-    };
-
-    const fetchMeat = async () => {
-        const resp = await fetch(`http://localhost:3001/api/meats`);
+  const fetchUser = async () => {
+    try {
+      const lstoken = localStorage.getItem("token");
+      if (lstoken) {
+        setToken(lstoken);
+      }
+      if (lstoken) {
+        const resp = await fetch(`http://localhost:3001/api/users/me`, {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${lstoken}`,
+          },
+        });
 
         const info = await resp.json();
 
-        setMeat(info);
-    };
+        if (info) {
+          setUser(info.user);
+        }
+        console.log(info);
+        return info;
+      }
+    } catch (error) {
+      throw error;
+    }
+  };
 
-    useEffect(() => {
-        fetchUser();
-        fetchMeat();
-    }, []);
+  const fetchMeat = async () => {
+    const resp = await fetch(`http://localhost:3001/api/meats`);
 
+    const info = await resp.json();
+
+    setMeat(info);
+  };
+
+  useEffect(() => {
+    fetchUser();
+    fetchMeat();
+  }, []);
+  if (user.administrator === true) {
     return (
-        <div id="container">
-            <Navbar user={user} />
-            <div id="main-section">
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-
-                    <Route
-                        exact
-                        path="/meat"
-                        element={<Meat meats={meats} />}
-                    />
-                    <Route
-                        exact
-                        path="/login"
-                        element={<Login user={user} setUser={setUser} />}
-                    />
-                    <Route
-                        exact
-                        path="/register"
-                        element={<Register user={user} setUser={setUser} />}
-                    />
-                    <Route exact path="/info" element={<Info />} />
-                </Routes>
-            </div>
-        </div>
+      <>
+        <h1>hello super user</h1>
+      </>
     );
+  } else {
+    return (
+      <div id="container">
+        <Navbar user={user} />
+        <div id="main-section">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+
+            <Route exact path="/meat" element={<Meat meats={meats} />} />
+            <Route
+              exact
+              path="/login"
+              element={<Login user={user} setUser={setUser} />}
+            />
+            <Route
+              exact
+              path="/register"
+              element={<Register user={user} setUser={setUser} />}
+            />
+            <Route exact path="/info" element={<Info />} />
+          </Routes>
+        </div>
+      </div>
+    );
+  }
 };
 export default App;
---
+++ src/components/admin.js
@@ -0,0 +1,9 @@
+const Admin = () => {
+  return (
+    <>
+      <h1>are you allowed here?</h1>
+    </>
+  );
+};
+
+export default Admin;
