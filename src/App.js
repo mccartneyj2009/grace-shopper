8a66d175138820e90edef050e57c2af319541685
James McCartney <mccartneyj2009@gmail.com>
2022-04-26 18:44:23 -0400

    lots of stuff

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 
-import { Meat, Home, Navbar, Login, Register, Info } from "./components";
+import { Meat, Home, Navbar, Login, Register, Info, Cart } from "./components";
 
 const App = () => {
     const [meats, setMeat] = useState([]);
     const [user, setUser] = useState({});
     const [token, setToken] = useState("");
+    const [tempCart, setTempCart] = useState([]);
 
     const fetchUser = async () => {
         try {
@@ -27,7 +28,6 @@ const App = () => {
                 if (info) {
                     setUser(info.user);
                 }
-                console.log(info);
                 return info;
             }
         } catch (error) {
@@ -58,7 +58,13 @@ const App = () => {
                     <Route
                         exact
                         path="/meat"
-                        element={<Meat meats={meats} />}
+                        element={
+                            <Meat
+                                meats={meats}
+                                tempCart={tempCart}
+                                setTempCart={setTempCart}
+                            />
+                        }
                     />
                     <Route
                         exact
@@ -69,6 +75,16 @@ const App = () => {
                         exact
                         path="/register"
                         element={<Register user={user} setUser={setUser} />}
+                    />
+                    <Route
+                        exact
+                        path="/cart"
+                        element={
+                            <Cart
+                                tempCart={tempCart}
+                                setTempCart={setTempCart}
+                            />
+                        }
                     />
                     <Route exact path="/info" element={<Info />} />
                 </Routes>
